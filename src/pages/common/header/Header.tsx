649ddc64a5cf54d048065eca7fe0bc025ef5adbf
Jan Jelínek <janjelinek.jr@gmail.com>
2023-06-21 21:01:27 +0200

    redesign header-colors, logo, changing logo on hover, header.module created, refacotr classes with clsx, update variables-custom.bootstrap

--- src/pages/common/header/Header.tsx
+++ src/pages/common/header/Header.tsx
@@ -1,25 +1,32 @@
-import {Container, Nav, Navbar} from "react-bootstrap";
-import './Header.scss'
-// import mainLogo from '../../../devdata/imgs/lam.svg'
+import {Badge, Container, Nav, Navbar} from "react-bootstrap";
+import clsx from "clsx";
+import {useState} from "react";
+
 const Header = () => {
+    const [switchHeader, setSwitchHeader] = useState(false)
+
     return (
-        <Navbar className='mb-5' bg="primary" variant="dark" expand={false}>
+        <Navbar className={clsx('d-flex', 'mb-5', 'mt-3')} bg="white" variant="light" expand={false}>
             <Container fluid='xxl'>
-                <div>
-                    <Navbar.Brand href="/">
-                        <img
-                            alt=""
-                            // src={mainLogo}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top me-3"
-                        />
-                        BAZZAR
+                <div className={clsx('d-flex')}>
+                    <Navbar.Brand href="/" onMouseEnter={() => {
+                        setSwitchHeader(true)
+                    }} onMouseLeave={() => {
+                        setSwitchHeader(false)
+                    }}>
+                        {!switchHeader ? <h3 className={clsx('mb-0', 'd-flex')}>
+                            <Badge>Vele</Badge><span
+                            className={clsx('text-primary', 'align-self-end', 'pb-1')}>Bazar</span>
+                        </h3> : <h3 className={clsx('mb-0', 'd-flex', 'gap-1')}>
+                            <span
+                                className={clsx('text-primary', 'align-self-end', 'pb-1')}>Vele</span><Badge>Bazar</Badge>
+                        </h3>
+                        }
                     </Navbar.Brand>
                 </div>
-                <div className='d-flex justify-content-end'>
-                    <Navbar.Toggle />
-                    <Navbar.Collapse className='navbar-expand order-first'>
+                <div className={clsx('d-flex', 'justify-content-end')}>
+                    <Navbar.Toggle/>
+                    <Navbar.Collapse className={clsx('navbar-expand', 'order-first')}>
                         <Nav>
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/">Link</Nav.Link>
