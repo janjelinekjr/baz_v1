224f72a3be8bc85ed9921e473548912eecce000a
Jan Jelínek <janjelinek.jr@gmail.com>
2023-06-24 10:26:36 +0200

    redesign of goodlist section, refactor and create styles module

--- src/pages/common/goodsList/GoodsList.tsx
+++ src/pages/common/goodsList/GoodsList.tsx
@@ -1,16 +1,19 @@
-import {Card, CardGroup, Form} from "react-bootstrap";
+import {Badge, Button, Card, CardGroup, Form} from "react-bootstrap";
 import {
     BazarTypeEnum,
     GoodsListType,
     SortEnum
 } from "../../../services/entities/Goods";
 import {formatAmount, formatDate} from "../../../utils/formatUtils";
-import './GoodsList.scss'
+import './GoodsList.module.scss'
 import sbazarLogo from '../../../devdata/imgs/logos/logo-sbazar.svg'
 import bazosLogo from '../../../devdata/imgs/logos/bazos.svg'
 import aukroLogo from '../../../devdata/imgs/logos/aukro.svg'
 import {useForm} from "react-hook-form";
 import {useAppSelector} from "../../../store/store";
+import clsx from "clsx";
+import styles from './GoodsList.module.scss'
+import {BoxArrowUpRight} from "react-bootstrap-icons";
 
 type GoodsListProps = {
     data: GoodsListType,
@@ -41,9 +44,9 @@ const GoodsList = ({data, indexOfFirstItem, indexOfLastItem}: GoodsListProps) =>
 
     return (
         <>
-            <div className='d-flex justify-content-end mt-4'>
-                <div className='d-flex align-items-center gap-4'>
-                    <Form.Label>Seřadit:</Form.Label>
+            <div className={clsx('d-flex', 'justify-content-end', 'mt-4')}>
+                <div className={clsx('d-flex', 'align-items-center', 'gap-4')}>
+                    <Form.Label className={clsx('text-primary', 'mb-0')}>Seřadit:</Form.Label>
                     <Form.Select {...register("sort")}>
                         <option>Vybrat řazení</option>
                         <option value={SortEnum.dateDesc}>Od nejnovějšího</option>
@@ -54,54 +57,63 @@ const GoodsList = ({data, indexOfFirstItem, indexOfLastItem}: GoodsListProps) =>
                 </div>
             </div>
             <div>
-                <Card className='mt-4'>
+                <Card className={clsx('mt-4', 'bg-light', 'shadow')}>
                     <Card.Body>
-                        <p>Celkový počet nalezených položek Sbazar: <span className='total-items-span'>{Number(totalItemsFound.sbazar).toLocaleString('cs-CZ')}</span></p>
-                        <p>Celkový počet nalezených položek Aukro: <span className='total-items-span'>{Number(totalItemsFound.aukro).toLocaleString('cs-CZ')}</span></p>
+                        <p className={clsx('text-primary')}>Celkový počet nalezených položek Sbazar: <span
+                            className={clsx(styles.totalItemsSpan)}>{Number(totalItemsFound.sbazar).toLocaleString('cs-CZ')}</span>
+                        </p>
+                        <p className={clsx('text-primary')}>Celkový počet nalezených položek Aukro: <span
+                            className={clsx(styles.totalItemsSpan)}>{Number(totalItemsFound.aukro).toLocaleString('cs-CZ')}</span>
+                        </p>
                     </Card.Body>
                 </Card>
             </div>
             <CardGroup
-                className='mt-3 row row-cols-xxl-4 row row-cols-xl-4 row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 row-cols-1 gx-2 gy-3'>
+                className={clsx('mt-3', 'row', 'row-cols-xxl-4', 'row', 'row-cols-xl-4', 'row', 'row-cols-lg-3', 'row-cols-md-2', 'row-cols-sm-1', 'row-cols-1', 'gx-2', 'gy-3')}>
                 {getDataWithSort()?.map(item => {
                     const bazarLogo =
                         item.bazar === BazarTypeEnum.sbazar ?
-                            <img className='sbazar-logo' src={sbazarLogo} alt='logo'/> :
+                            <img className={clsx(styles.sbazarLogo)} src={sbazarLogo} alt='logo'/> :
                             item.bazar === BazarTypeEnum.bazos ?
-                                <img className='bazos-logo' src={bazosLogo} alt='logo'/> :
+                                <img className={clsx(styles.bazosLogo)} src={bazosLogo} alt='logo'/> :
                                 item.bazar === BazarTypeEnum.aukro ?
-                                    <img className='aukro-logo' src={aukroLogo} alt='logo'/> : null
+                                    <img className={clsx(styles.aukroLogo)} src={aukroLogo} alt='logo'/> : null
                     const priceResult = (item.price === 0 && !item.price_by_agree) ? 'Zdarma' : item.price_by_agree ? 'Dohodou' : formatAmount(item.price)
                     const originUrl: string = item.bazar === BazarTypeEnum.sbazar ? `https://www.sbazar.cz/${item.user}/detail/${item.seo_name}` : item.bazar === BazarTypeEnum.aukro ? `https://www.aukro.cz/${item.seo_name}-${item.id}` : ''
 
                     return (
                         <div key={item.id} className='col'>
-                            <Card className='cards h-100'>
-                                <Card.Img variant="top" className='cards-img' src={item.titleImg}/>
-                                <Card.Body>
-                                    <Card.Title>{item.name}</Card.Title>
+                            <Card className={clsx(styles.cards, 'h-100', 'shadow')}>
+                                <Card.Img variant="top" className={clsx(styles.cardsImg)} src={item.titleImg}/>
+                                <Card.Body className={clsx('d-flex', 'flex-column', 'justify-content-between')}>
+                                    <Card.Title className={clsx('text-primary')}>{item.name}</Card.Title>
                                     <Card.Text>
-                                        <div className='d-flex justify-content-between mb-4'>
-                                            <span>Přidáno:</span>
-                                            <span>{formatDate(item.startTime)}</span>
-                                        </div>
-                                        <div className='text-center mb-2'>
-                                            <span>{bazarLogo}</span>
-                                        </div>
-                                        <div className='text-center'>
-                                            <a className='transfer-link'
-                                               href={originUrl}
-                                               target="_blank">
-                                                Přejít na inzerát
-                                            </a>
+                                        <div className={clsx('d-flex', 'flex-column', 'gap-3', 'mb-3')}>
+                                            <div className={clsx('d-flex', 'justify-content-between', 'mb-2')}>
+                                                <span className={clsx('text-primary')}>Přidáno:</span>
+                                                <span
+                                                    className={clsx('text-primary')}>{formatDate(item.startTime)}</span>
+                                            </div>
+                                            <div className={clsx('text-center', 'mb-2')}>
+                                                <span>{bazarLogo}</span>
+                                            </div>
+                                            <div className='text-center'>
+                                                <Button as={'a'} className={clsx('text-light')} href={originUrl}
+                                                        target='_blank'>
+                                                    <span
+                                                        className={clsx('d-flex', 'gap-2', 'align-items-center')}><span
+                                                        className={clsx('mb-0')}>Přejít na inzerát</span> <BoxArrowUpRight
+                                                        className={clsx('mb-1')}/></span>
+                                                </Button>
+                                            </div>
                                         </div>
                                     </Card.Text>
                                 </Card.Body>
-                                <Card.Footer>
+                                <Card.Footer className={clsx('bg-white')}>
                                     <small className="text-muted">
-                                        <div className='d-flex justify-content-between'>
-                                            <span>{item.location.location || item.location.region}</span>
-                                            <span className='fw-bold'>{priceResult}</span>
+                                        <div className={clsx('d-flex', 'justify-content-between')}>
+                                            <span className={clsx('text-primary', 'fw-semibold', 'align-self-center')}>{item.location.location || item.location.region}</span>
+                                            <Badge className={clsx('fw-bold', 'bg-light', 'text-primary', 'd-flex', 'align-items-center', 'pe-3', 'ps-3', 'fs-6', 'shadow-sm')}>{priceResult}</Badge>
                                         </div>
                                     </small>
                                 </Card.Footer>
